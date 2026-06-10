80ac32ae0603810af36b4602c3aa09daf6658474
Levi Cai <levi.cai@colorado.edu>
2026-06-10 15:41:55 -0600

    Commented out A100 specific code

--- satclip/main.py
+++ satclip/main.py
@@ -168,9 +168,9 @@ if __name__ == "__main__":
     config_fn = "./configs/default.yaml"
 
     #A100 go vroom vroom 🚗💨
-    if torch.cuda.get_device_name(device=0)=='NVIDIA A100 80GB PCIe':
-        torch.backends.cuda.matmul.allow_tf32 = True
-        print('Superfastmode! 🚀')
-    else:
-        torch.backends.cuda.matmul.allow_tf32 = False
+    # if torch.cuda.get_device_name(device=0)=='NVIDIA A100 80GB PCIe':
+    #     torch.backends.cuda.matmul.allow_tf32 = True
+    #     print('Superfastmode! 🚀')
+    # else:
+    #     torch.backends.cuda.matmul.allow_tf32 = False
     cli_main(config_fn)
